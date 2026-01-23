0fa70263783d281ff603a764c49f14b0ff179878
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-01-23 15:03:50 +0100

    add create primitive object with initial LunyScript tests

---
+++ LunyScriptExecutionTests.cs
@@ -0,0 +1,32 @@
+﻿using Luny;
+using Luny.Test;
+using NUnit.Framework;
+using System;
+
+namespace LunyScript.Test
+{
+	public sealed class LunyScriptExecutionTestScript : LunyScript
+	{
+		public static Boolean DidRunBuild { get; private set; }
+		public override void Build() => DidRunBuild = true;
+	}
+
+	[TestFixture]
+	public sealed class LunyScriptExecutionTests : LunyTestBase
+	{
+		[Test]
+		public void LunyScript_RunsBuildMethod()
+		{
+			var adapter = CreateEngineMockAdapter();
+
+			var nativeObject = new MockNativeObject(nameof(LunyScriptExecutionTestScript));
+			var mockObject = new MockLunyObject(nativeObject);
+			var sceneService = (MockSceneService)LunyEngine.Instance.Scene;
+			sceneService.AddSceneObject(mockObject);
+
+			Assert.That(LunyScriptExecutionTestScript.DidRunBuild, Is.False);
+			adapter.Run();
+			Assert.That(LunyScriptExecutionTestScript.DidRunBuild, Is.True);
+		}
+	}
+}
---
+++ LunyScriptObjectTests.cs
@@ -0,0 +1,41 @@
+﻿using Luny;
+using Luny.Test;
+using NUnit.Framework;
+
+namespace LunyScript.Test
+{
+	public sealed class LunyScriptObjectTestScript : LunyScript
+	{
+		public override void Build()
+		{
+			When.Object.Ready(Object.CreateEmpty("empty"));
+			When.Object.Ready(Object.CreateCube("cube"));
+			When.Object.Ready(Object.CreateSphere("sphere"));
+		}
+	}
+
+	[TestFixture]
+	public sealed class LunyScriptObjectTests : LunyTestBase
+	{
+		[Test]
+		public void CreateObject_CallsServiceMethods()
+		{
+			LunyLogger.EnableInternalLogging = true;
+			var adapter = CreateEngineMockAdapter();
+
+			var nativeObject = new MockNativeObject(nameof(LunyScriptObjectTestScript));
+			var testObject = new MockLunyObject(nativeObject);
+			var sceneService = (MockSceneService)LunyEngine.Instance.Scene;
+			sceneService.AddSceneObject(testObject);
+
+			var objectService = LunyEngine.Instance.Object as MockObjectService;
+			Assert.That(objectService, Is.Not.Null, "MockObjectService should have been discovered and registered.");
+
+			adapter.Run();
+
+			Assert.That(objectService.Log, Contains.Item("CreateEmpty(empty)"));
+			Assert.That(objectService.Log, Contains.Item("CreatePrimitive(Cube,cube)"));
+			Assert.That(objectService.Log, Contains.Item("CreatePrimitive(Sphere,sphere)"));
+		}
+	}
+}
