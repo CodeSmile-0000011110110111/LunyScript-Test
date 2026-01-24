59eb3c8f9bb272e318eccb6bc1771e9b0544cf4a
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-01-24 13:42:00 +0100

    update blocks to 'one block per purpose'

--- LunyScriptObjectTests.cs
+++ LunyScriptObjectTests.cs
@@ -8,9 +8,13 @@ namespace LunyScript.Test
 	{
 		public override void Build()
 		{
-			When.Self.Ready(Object.CreateEmpty("empty"));
+			When.Self.Ready(Object.Create("empty"));
 			When.Self.Ready(Object.CreateCube("cube"));
 			When.Self.Ready(Object.CreateSphere("sphere"));
+			When.Self.Ready(Object.CreateCapsule("capsule"));
+			When.Self.Ready(Object.CreateCylinder("cylinder"));
+			When.Self.Ready(Object.CreatePlane("plane"));
+			When.Self.Ready(Object.CreateQuad("quad"));
 		}
 	}
 
@@ -31,6 +35,10 @@ namespace LunyScript.Test
 			Assert.That(objectService.Log, Contains.Item("CreateEmpty(empty)"));
 			Assert.That(objectService.Log, Contains.Item("CreatePrimitive(Cube,cube)"));
 			Assert.That(objectService.Log, Contains.Item("CreatePrimitive(Sphere,sphere)"));
+			Assert.That(objectService.Log, Contains.Item("CreatePrimitive(Capsule,capsule)"));
+			Assert.That(objectService.Log, Contains.Item("CreatePrimitive(Cylinder,cylinder)"));
+			Assert.That(objectService.Log, Contains.Item("CreatePrimitive(Plane,plane)"));
+			Assert.That(objectService.Log, Contains.Item("CreatePrimitive(Quad,quad)"));
 		}
 	}
 }
