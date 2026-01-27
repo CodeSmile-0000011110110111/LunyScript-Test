df97d39914ff5284419171ff28c755c6a0624a7c
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-01-27 20:33:43 +0100

    move tests into projects, apply shared style and names

---
+++ ObjectCreateTests.cs
@@ -0,0 +1,73 @@
+using Godot;
+using Luny;
+using Luny.ContractTest;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityObject = UnityEngine.Object;
+
+namespace LunyScript.Test
+{
+	public sealed class LunyScript_Test_CreateObjects : LunyScript
+	{
+		public override void Build()
+		{
+			When.Self.Ready(Object.Create("empty"));
+			When.Self.Ready(Object.CreateCube("cube"));
+			When.Self.Ready(Object.CreateSphere("sphere"));
+			When.Self.Ready(Object.CreateCapsule("capsule"));
+			When.Self.Ready(Object.CreateCylinder("cylinder"));
+			When.Self.Ready(Object.CreatePlane("plane"));
+			When.Self.Ready(Object.CreateQuad("quad"));
+		}
+	}
+
+	public abstract class ObjectCreateTests : ContractTestBase
+	{
+		[Test]
+		public void CreateObject_CreatesNativeObjects()
+		{
+			// We need to create the object with the same name as the script.
+			// The LunyScriptRunner will find it during OnSceneLoaded.
+			LunyEngine.Instance.Object.CreateEmpty(nameof(LunyScript_Test_CreateObjects));
+
+			SimulateFrame();
+
+			// Verify native objects exist
+			AssertNativeObjectExists("empty");
+			AssertNativeObjectExists("cube");
+			AssertNativeObjectExists("sphere");
+			AssertNativeObjectExists("capsule");
+			AssertNativeObjectExists("cylinder");
+			AssertNativeObjectExists("plane");
+			AssertNativeObjectExists("quad");
+		}
+
+		protected abstract void AssertNativeObjectExists(String name);
+	}
+
+	[TestFixture]
+	public sealed class GodotObjectCreateTests : ObjectCreateTests
+	{
+		protected override NativeEngine Engine => NativeEngine.Godot;
+
+		protected override void AssertNativeObjectExists(String name)
+		{
+			var node = GodotObject._allObjects.OfType<Node>().FirstOrDefault(n => n.Name == name);
+			Assert.That(node, Is.Not.Null, $"Native Godot Node '{name}' should exist.");
+		}
+	}
+
+	[TestFixture]
+	public sealed class UnityObjectCreateTests : ObjectCreateTests
+	{
+		protected override NativeEngine Engine => NativeEngine.Unity;
+
+		protected override void AssertNativeObjectExists(String name)
+		{
+			var go = UnityObject._allObjects.OfType<GameObject>().FirstOrDefault(g => g.name == name);
+			Assert.That(go, Is.Not.Null, $"Native Unity GameObject '{name}' should exist.");
+		}
+	}
+}
---
+++ ScriptLifecycleTests.cs
@@ -0,0 +1,71 @@
+using Luny;
+using Luny.ContractTest;
+using NUnit.Framework;
+
+namespace LunyScript.Test
+{
+	public sealed class SelfDisabled_DoesNotRunUpdates_LunyScript : LunyScript
+	{
+		public override void Build()
+		{
+			When.Self.Ready(Object.Disable());
+			When.Self.Steps(Method.Run(() => GlobalVars[nameof(When.Self.Steps)] = true));
+			When.Self.Updates(Method.Run(() => GlobalVars[nameof(When.Self.Updates)] = true));
+			When.Self.LateUpdates(Method.Run(() => GlobalVars[nameof(When.Self.LateUpdates)] = true));
+		}
+	}
+
+	public sealed class SelfEnabled_RunsUpdates_LunyScript : LunyScript
+	{
+		public override void Build()
+		{
+			When.Self.Ready(Object.Disable(), Object.Enable());
+			When.Self.Steps(Method.Run(() => GlobalVars[nameof(When.Self.Steps)] = true));
+			When.Self.Updates(Method.Run(() => GlobalVars[nameof(When.Self.Updates)] = true));
+			When.Self.LateUpdates(Method.Run(() => GlobalVars[nameof(When.Self.LateUpdates)] = true));
+		}
+	}
+
+	public abstract class ScriptLifecycleTests : ContractTestBase
+	{
+		[Test]
+		public void SelfDisabled_DoesNotRunUpdates()
+		{
+			LunyEngine.Instance.Object.CreateEmpty(nameof(SelfDisabled_DoesNotRunUpdates_LunyScript));
+			var gVars = LunyScriptEngine.Instance.GlobalVars;
+
+			SimulateFrames(3);
+
+			// not using Is.False: NUnit calls false.Equals({Variable}) which will always be false
+			Assert.That(gVars[nameof(LunyScript.When.Self.Steps)], Is.EqualTo(false));
+			Assert.That(gVars[nameof(LunyScript.When.Self.Updates)], Is.EqualTo(false));
+			Assert.That(gVars[nameof(LunyScript.When.Self.LateUpdates)], Is.EqualTo(false));
+		}
+
+		[Test]
+		public void SelfEnabled_RunsUpdates()
+		{
+			LunyEngine.Instance.Object.CreateEmpty(nameof(SelfEnabled_RunsUpdates_LunyScript));
+			var gVars = LunyScriptEngine.Instance.GlobalVars;
+
+			SimulateFrames(3);
+
+			// not using Is.True: NUnit calls false.Equals({Variable}) which will always be false
+			Assert.That(gVars[nameof(LunyScript.When.Self.Steps)], Is.EqualTo(true));
+			Assert.That(gVars[nameof(LunyScript.When.Self.Updates)], Is.EqualTo(true));
+			Assert.That(gVars[nameof(LunyScript.When.Self.LateUpdates)], Is.EqualTo(true));
+		}
+	}
+
+	[TestFixture]
+	public sealed class GodotScriptLifecycleTests : ScriptLifecycleTests
+	{
+		protected override NativeEngine Engine => NativeEngine.Godot;
+	}
+
+	[TestFixture]
+	public sealed class UnityScriptLifecycleTests : ScriptLifecycleTests
+	{
+		protected override NativeEngine Engine => NativeEngine.Unity;
+	}
+}
