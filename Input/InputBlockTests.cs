0b0718220a7077257c55168fbba7b0d7a635bc26
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-18 16:35:31 +0100

    input event handling, first pass

---
+++ Input/InputBlockTests.cs
@@ -0,0 +1,192 @@
+﻿using Luny;
+using Luny.ContractTest;
+using Luny.Engine.Bridge;
+using Luny.Unity.Engine.Services;
+using LunyScript.Blocks;
+using NUnit.Framework;
+using System;
+
+namespace LunyScript.Test.Input
+{
+	public sealed class InputButtonValueTestScript : Script
+	{
+		public override void Build(ScriptContext context)
+		{
+			On.FrameUpdate(
+				GVar["btn_value"].Set(Input.Button("Fire").Value)
+			);
+		}
+	}
+
+	[TestFixture]
+	public sealed class InputAxisBlockTests : ContractTestBase
+	{
+		protected override NativeEngine Engine => NativeEngine.Unity;
+
+		private UnityInputService InputService => (UnityInputService)LunyEngine.Instance.Input;
+
+		[Test]
+		public void Axis_Returns_Zero_When_No_Input()
+		{
+			var block = InputAxisBlock.Create("Move");
+			var value = block.GetValue(null);
+
+			Assert.That(value.Type, Is.EqualTo(Variable.ValueType.Vector2));
+			Assert.That(value.AsVector2(), Is.EqualTo(LunyVector2.Zero));
+		}
+
+		[Test]
+		public void Axis_Returns_Value_After_Simulate()
+		{
+			var expected = new LunyVector2(0.7f, -0.3f);
+			InputService.SimulateAxisInput("Move", expected);
+
+			var block = InputAxisBlock.Create("Move");
+			var value = block.GetValue(null);
+
+			Assert.That(value.AsVector2(), Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void Axis_GetValueGeneric_Returns_LunyVector2()
+		{
+			var expected = new LunyVector2(1f, 0f);
+			InputService.SimulateAxisInput("Move", expected);
+
+			var block = InputAxisBlock.Create("Move");
+			var vec = block.GetValue<LunyVector2>(null);
+
+			Assert.That(vec, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void Axis_Persists_Across_Frames()
+		{
+			var expected = new LunyVector2(0.5f, 0.5f);
+			InputService.SimulateAxisInput("Look", expected);
+
+			SimulateFrames(3);
+
+			var block = InputAxisBlock.Create("Look");
+			Assert.That(block.GetValue<LunyVector2>(null), Is.EqualTo(expected));
+		}
+	}
+
+	[TestFixture]
+	public sealed class InputIsPressedBlockTests : ContractTestBase
+	{
+		protected override NativeEngine Engine => NativeEngine.Unity;
+
+		private UnityInputService InputService => (UnityInputService)LunyEngine.Instance.Input;
+
+		[Test]
+		public void IsPressed_False_When_No_Input()
+		{
+			var block = InputIsPressedBlock.Create("Fire");
+			Assert.That(block.Evaluate(null), Is.False);
+		}
+
+		[Test]
+		public void IsPressed_True_While_Held()
+		{
+			InputService.SimulateButtonInput("Fire", true);
+
+			var block = InputIsPressedBlock.Create("Fire");
+			Assert.That(block.Evaluate(null), Is.True);
+		}
+
+		[Test]
+		public void IsPressed_False_After_Release()
+		{
+			InputService.SimulateButtonInput("Fire", true);
+			InputService.SimulateButtonInput("Fire", false);
+
+			var block = InputIsPressedBlock.Create("Fire");
+			Assert.That(block.Evaluate(null), Is.False);
+		}
+	}
+
+	[TestFixture]
+	public sealed class InputIsJustPressedBlockTests : ContractTestBase
+	{
+		protected override NativeEngine Engine => NativeEngine.Unity;
+
+		private UnityInputService InputService => (UnityInputService)LunyEngine.Instance.Input;
+
+		[Test]
+		public void IsJustPressed_False_When_No_Input()
+		{
+			var block = InputIsJustPressedBlock.Create("Jump");
+			Assert.That(block.Evaluate(null), Is.False);
+		}
+
+		[Test]
+		public void IsJustPressed_True_On_Press_Frame()
+		{
+			InputService.SimulateButtonInput("Jump", true);
+
+			var block = InputIsJustPressedBlock.Create("Jump");
+			Assert.That(block.Evaluate(null), Is.True);
+		}
+
+		[Test]
+		public void IsJustPressed_False_After_PreUpdate()
+		{
+			InputService.SimulateButtonInput("Jump", true);
+			SimulateFrames(1);
+
+			var block = InputIsJustPressedBlock.Create("Jump");
+			Assert.That(block.Evaluate(null), Is.False);
+		}
+	}
+
+	[TestFixture]
+	public sealed class InputButtonValueBlockTests : ContractTestBase
+	{
+		protected override NativeEngine Engine => NativeEngine.Unity;
+
+		private UnityInputService InputService => (UnityInputService)LunyEngine.Instance.Input;
+
+		[Test]
+		public void ButtonValue_Zero_When_No_Input()
+		{
+			var block = InputButtonValueBlock.Create("Trigger");
+			Assert.That(block.GetValue(null).AsDouble(), Is.EqualTo(0.0));
+		}
+
+		[Test]
+		public void ButtonValue_Returns_Analog()
+		{
+			InputService.SimulateButtonInput("Trigger", true, 0.75f);
+
+			var block = InputButtonValueBlock.Create("Trigger");
+			Assert.That(block.GetValue(null).AsDouble(), Is.EqualTo(0.75).Within(0.01));
+		}
+
+		[Test]
+		public void ButtonValue_In_Script_Via_GVar()
+		{
+			LunyEngine.Instance.Object.CreateEmpty(nameof(InputButtonValueTestScript));
+			InputService.SimulateButtonInput("Fire", true, 0.6f);
+
+			SimulateFrames(1);
+
+			var gVars = ScriptEngine.Instance.GlobalVariables;
+			Assert.That(gVars["btn_value"].AsDouble(), Is.EqualTo(0.6).Within(0.01));
+		}
+
+		[Test]
+		public void ButtonValue_In_Script_Zero_After_Release()
+		{
+			LunyEngine.Instance.Object.CreateEmpty(nameof(InputButtonValueTestScript));
+			InputService.SimulateButtonInput("Fire", true, 0.8f);
+			SimulateFrames(1);
+
+			InputService.SimulateButtonInput("Fire", false);
+			SimulateFrames(1);
+
+			var gVars = ScriptEngine.Instance.GlobalVariables;
+			Assert.That(gVars["btn_value"].AsDouble(), Is.EqualTo(0.0));
+		}
+	}
+}
---
+++ Variables/ComputedVariableBlockTests.cs
@@ -0,0 +1,137 @@
+using Luny;
+using LunyScript.Blocks;
+using LunyScript.Exceptions;
+using NUnit.Framework;
+using System;
+
+namespace LunyScript.Test.Variables
+{
+	[TestFixture]
+	public sealed class ComputedVariableBlockTests
+	{
+		private sealed class TestComputedBlock : ComputedVariableBlock
+		{
+			private readonly Variable _value;
+
+			internal static TestComputedBlock Create(Variable value) => new(value);
+			private TestComputedBlock(Variable value) => _value = value;
+
+			public override Variable GetValue(IScriptRuntimeContext runtimeContext) => _value;
+		}
+
+		private sealed class TestVector2ComputedBlock : ComputedVariableBlock
+		{
+			private readonly LunyVector2 _value;
+
+			internal static TestVector2ComputedBlock Create(LunyVector2 value) => new(value);
+			private TestVector2ComputedBlock(LunyVector2 value) => _value = value;
+
+			public override Variable GetValue(IScriptRuntimeContext runtimeContext) =>
+				Variable.FromVector2(_value);
+
+			public override T GetValue<T>(IScriptRuntimeContext runtimeContext)
+			{
+				if (typeof(T) == typeof(LunyVector2))
+				{
+					var val = _value;
+					return System.Runtime.CompilerServices.Unsafe.As<LunyVector2, T>(ref val);
+				}
+				return base.GetValue<T>(runtimeContext);
+			}
+		}
+
+		[Test]
+		public void TargetHandle_Is_Null()
+		{
+			var block = TestComputedBlock.Create(42);
+			Assert.That(block.TargetHandle, Is.Null);
+		}
+
+		[Test]
+		public void Set_Throws_ReadOnly_Exception()
+		{
+			var block = TestComputedBlock.Create(42);
+			Assert.Throws<LunyScriptVariableException>(() => block.Set(10));
+		}
+
+		[Test]
+		public void Inc_Throws_ReadOnly_Exception()
+		{
+			var block = TestComputedBlock.Create(42);
+			Assert.Throws<LunyScriptVariableException>(() => block.Inc());
+		}
+
+		[Test]
+		public void Add_Throws_ReadOnly_Exception()
+		{
+			var block = TestComputedBlock.Create(42);
+			Assert.Throws<LunyScriptVariableException>(() => block.Add(1));
+		}
+
+		[Test]
+		public void GetValue_Returns_Correct_Value()
+		{
+			var block = TestComputedBlock.Create(42);
+			Assert.That(block.GetValue(null).AsDouble(), Is.EqualTo(42.0));
+		}
+
+		[Test]
+		public void GetValueGeneric_Double_Returns_Correct_Value()
+		{
+			var block = TestComputedBlock.Create(42);
+			Assert.That(block.GetValue<Double>(null), Is.EqualTo(42.0));
+		}
+
+		[Test]
+		public void GetValueGeneric_Single_Returns_Correct_Value()
+		{
+			var block = TestComputedBlock.Create(42);
+			Assert.That(block.GetValue<Single>(null), Is.EqualTo(42f));
+		}
+
+		[Test]
+		public void GetValueGeneric_Boolean_Returns_Correct_Value()
+		{
+			var block = TestComputedBlock.Create(true);
+			Assert.That(block.GetValue<Boolean>(null), Is.True);
+		}
+
+		[Test]
+		public void GetValueGeneric_Int32_Returns_Correct_Value()
+		{
+			var block = TestComputedBlock.Create(42);
+			Assert.That(block.GetValue<Int32>(null), Is.EqualTo(42));
+		}
+
+		[Test]
+		public void GetValueGeneric_Vector2_Returns_Correct_Value()
+		{
+			var vec = new LunyVector2(3f, 4f);
+			var block = TestVector2ComputedBlock.Create(vec);
+			Assert.That(block.GetValue<LunyVector2>(null), Is.EqualTo(vec));
+		}
+
+		[Test]
+		public void GetValueGeneric_Vector2_Via_BaseDefault()
+		{
+			var vec = new LunyVector2(3f, 4f);
+			var block = TestComputedBlock.Create(Variable.FromVector2(vec));
+			Assert.That(block.GetValue<LunyVector2>(null), Is.EqualTo(vec));
+		}
+
+		[Test]
+		public void GetValueGeneric_Vector3_Via_BaseDefault()
+		{
+			var vec = new LunyVector3(1f, 2f, 3f);
+			var block = TestComputedBlock.Create(Variable.FromVector3(vec));
+			Assert.That(block.GetValue<LunyVector3>(null), Is.EqualTo(vec));
+		}
+
+		[Test]
+		public void GetValueGeneric_Unsupported_Type_Throws()
+		{
+			var block = TestComputedBlock.Create(42);
+			Assert.Throws<LunyScriptVariableException>(() => block.GetValue<LunyQuaternion>(null));
+		}
+	}
+}
